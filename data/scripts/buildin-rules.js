5a51cb3e2f775b6af9b44c3cac0d090c790d00c7
Sebastian Stöhr <sdstoehr@gmail.com>
2012-09-17 15:39:49 +0200

    new buildin rule: duplicate id

--- data/scripts/buildin-rules.js
+++ data/scripts/buildin-rules.js
@@ -18,3 +18,48 @@ SiteCheckr.buildinRules.demoTest.prototype.custom_check = function(contextNode) 
     // check something
     return result;
 };
+
+/**************************************
+ *         DUPLICATE ID
+ *************************************/
+SiteCheckr.buildinRules.duplicateId = function() {
+    this.init("", SiteCheckr.constants.MODE.REQUIRED, SiteCheckr.constants.TYPE.CUSTOM);
+};
+
+SiteCheckr.buildinRules.duplicateId.prototype = SiteCheckr.rules.test.prototype;
+SiteCheckr.buildinRules.duplicateId.prototype.custom_check = function(contextNode) {
+    var result = { boolean: true, elements: [] };
+
+    var ids = {}; //store ID => #num
+
+    //get all elements with an id:
+    var xpath = document.evaluate('//*[@id]', contextNode, null, XPathResult.ANY_TYPE, null);
+    var elem = xpath.iterateNext();
+    if(elem) {
+
+        var id;
+
+        while(elem) {
+            id = elem.id;
+
+            if(!id) {
+                elem = xpath.iterateNext();
+                continue;
+            }
+
+            if(ids[id] && ids[id] > 0) {
+                ids[id] += 1;
+                result.elements.push(SiteCheckr.Helper.getDOMElementAsJSON(elem));
+                result.boolean = false;
+            } else {
+                ids[id] = 1;
+            }
+
+            elem = xpath.iterateNext();
+        }
+
+        elem = null;
+    }
+
+    return result;
+};
